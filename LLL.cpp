879b4673880fe5f5889af29aa064b6178f2c35cb
Nam Dinh <dinhhainam2013@yahoo.com>
2014-11-04 18:14:49 +0700

    Initiate remove function

--- LLL.cpp
+++ LLL.cpp
@@ -33,8 +33,7 @@ int display(node * head){
             }
             cout << "\n\n";
             return 0;
-        }
-        
+        }        
     }
 }
 
@@ -116,6 +115,129 @@ int add(node *& head){
     }
 }
 
+int remove_by_name(node *& head, char * name){
+    if (!head){
+        cout << "No student in list." << endl;
+        return 1;
+    }
+    else{
+        if (!head->next){
+            if (strcmp(head->name, name) == 0){
+                delete head;
+                head = NULL;
+                return 0;
+            }
+            else {
+                cout << "Student with name " << name << " is not found" << endl;
+                return 1;
+            }
+        }
+        else{
+            if(head->name == name){
+                node * temp = head;
+                head = head->next;
+                delete temp;
+                temp = NULL;
+                return 0;
+            } else {
+                node * current = head->next;
+                node * prev = head;
+                while((strcmp(current->name, name) !=0) && current){
+                    current = current->next;
+                    prev = prev->next;
+                }
+                if (!current){
+                    cout << "There is no student with name " << name << endl;
+                    return 1;
+                }
+                else {
+                    prev->next = current->next;
+                    delete current;
+                    current = NULL;
+                    return 0;
+                }
+            }
+        }
+    }   
+}
+
+int remove_by_id(node *& head, int id){
+    if (!head){
+        cout << "No student in list." << endl;
+        return 1;
+    }
+    else{
+        if (!head->next){
+            if (head->id == id){
+                delete head;
+                head = NULL;
+                return 0;
+            }
+            else {
+                cout << "Student with id " << id  << " is not found" << endl;
+                return 1;
+            }
+        }
+        else{
+            node * current = head->next;
+            node * prev = head;
+            while((current->id != id) && current){
+                current = current->next;
+                prev = prev->next;
+            }
+            if (!current){
+                cout << "There is no student with id " << id << endl;
+                return 1;
+            }
+            //ADD AN IF HERE LIKE remove_by_name
+            else {
+                prev->next = current->next;
+                delete current;
+                current = NULL;
+                return 0;
+            }
+        }
+    }   
+}
+
+int remove(node *& head){
+    int choice = 0;
+    char t_name[20];
+    int t_id = 0;
+
+    cout << "1. Remove by name.";
+    cout << "\n2. Remove by id.";
+    cout << "\n3. Cancel.";
+    cout << "\nRemove move by: ";
+
+    cin >> choice;
+    cin.ignore(100, '\n');
+    
+    while((choice < 1) || (choice >2)) {
+        cout << "Cant recognize the remove option: ";
+        cout << "\nPlease pick the existent remove option(1-2)" << endl;
+        cout << "Remove by: " ;
+        cin >> choice;
+        cin.ignore(100, '\n');
+    }
+    if (choice == 1) {
+        cout << "Please enter the student's name: ";
+        cin >> t_name;
+        cin.ignore(100,'\n');
+        remove_by_name(head, t_name);
+        return 0;
+    } else if (choice == 2){
+        cout << "Please enter the student's id: ";
+        cin >> t_id;
+        cin.ignore(100, '\n');
+        remove_by_id(head, t_id);
+        return 0;
+    } else {
+        cout << "Remove process canceled." << endl;
+        return 0;
+    }
+}
+
 int menu(node *& head){
     cout << "1. Add a new student." << endl;
     cout << "2. Delete a student." << endl;
@@ -140,7 +262,12 @@ int menu(node *& head){
     } else if (choice == 4){
         cout << "Thanks!" << endl;
         return 4;
-    } else cout << "The option is underconstruction." << endl << endl;
+    } else if (choice == 2) {
+        remove(head);
+        display(head);
+        return 2;
+    }   else cout << "The option is underconstruction." << endl << endl;
+    
     return 0;
 }
 
@@ -152,9 +279,10 @@ int main(){
     while (menu(head) != 4){
         ++deb_int;
     }
-
+    
     cin.get();
     cout << "Chao tam biet" << endl;
+    cout  << deb_int << " operations" << endl;
     return 0;
 }
 
